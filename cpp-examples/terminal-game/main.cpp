f9ed6036ea64c68993ff5903a9d9ad610382f7ab
Dmitry Savenko <ds@dsavenko.com>
2023-11-01 19:38:28 +0700

    Finish the game example

--- cpp-examples/terminal-game/main.cpp
+++ cpp-examples/terminal-game/main.cpp
@@ -5,9 +5,24 @@
 #include <curses.h>
 
 #include <chrono>
-#include <thread>
+#include <vector>
+#include <algorithm>
+
+using namespace std::chrono_literals;
+
+using steady_clock_t = std::chrono::time_point<std::chrono::steady_clock>;
+
+void out(int y, int x, const std::string& s) {
+    wmove(stdscr, y, x);
+    wprintw(stdscr, s.c_str());
+}
+
+steady_clock_t now() {
+    return std::chrono::steady_clock::now();
+}
 
 int main() {
+    setlocale(LC_ALL, "");
 
     initscr();
     raw();
@@ -20,42 +35,53 @@ int main() {
 
     int h1, w1;
     getmaxyx(stdscr, h1, w1);
-    int h = h1, w = w1;
+    int h = h1/2;
+    int w = w1/2;
 
-    wmove(stdscr, h / 2, w / 2);
-    wprintw(stdscr, "hello from %s, rows %d, cols %d", "curses", h1, w1);
+    struct Bullet {
+        int w;
+        int h;
+        steady_clock_t last_time;
+    };
+    std::vector<Bullet> bullets;
 
     int c;
-    bool cont = false;
-    while ('q' != (c = getch())) {  
-        cont = false;      
+    int z = 0;
+    while ('q' != (c = getch())) {
+        clear();
+        for (Bullet& b : bullets) {
+            if ((now() - b.last_time) / 1ms > 300) {
+                b.h--;
+                b.last_time = now();
+            }
+            out(b.h, b.w, "\u25cf");
+        }
+        bullets.erase(
+            std::remove_if(bullets.begin(), bullets.end(), [](const Bullet& b) { return b.h < 2; }),
+            bullets.end()
+        );
+        for (int i = 1; i < w1; ++i) {
+            out(0, i, "\u2501");
+            out(h1-1, i, "\u2501");
+        }
+        for (int i = 1; i < h1-1; ++i) {
+            out(i, 0, "\u2503");
+            out(i, w1-1, "\u2503");
+        }
+        out(0, 0, "\u250f");
+        out(0, w1-1, "\u2513");
+        out(h1-1, 0, "\u2517");
+        out(h1-1, w1-1, "\u251b");
+
         switch(c) {
         case KEY_LEFT: --w; break;
         case KEY_RIGHT: ++w; break;
         case KEY_UP: --h; break;
         case KEY_DOWN: ++h; break;
-        default: cont = true; break;
+        case ' ': bullets.push_back(Bullet{w, h, now()}); break;
         }
         flushinp();
-        if (cont) {
-            std::this_thread::sleep_for(std::chrono::nanoseconds(10000));
-            continue;
-        }
-        if (0 >= h) {
-            h = 1;
-        }
-        if (0 >= w) {
-            w = 1;
-        }
-        if (w >= w1) {
-            w = w1-1;
-        }
-        if (h >= h1) {
-            h = h1-1;
-        }
-        clear();
-        wmove(stdscr, h / 2, w / 2);
-        wprintw(stdscr, "hello from %s, rows %d/%d, cols %d/%d", "curses", h, h1, w, w1);
+        out(h, w, "\u2588");
         refresh();
     }
 
